92c1cbe0fd24cc8163c74d3113954cf9f56edd56
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-02-01 12:24:50 +0200

    add main pagge template

--- app/src/main/java/hexlet/code/controllers/RootController.java
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -4,8 +4,12 @@ import java.io.PrintWriter;
 import io.javalin.http.Handler;
 
 public class RootController {
+//    public static Handler welcome = ctx -> {
+//        PrintWriter printWriter = ctx.res().getWriter();
+//        printWriter.write("Hello World\nby Andrey Karelskiy");
+//    };
+
     public static Handler welcome = ctx -> {
-        PrintWriter printWriter = ctx.res().getWriter();
-        printWriter.write("Hello World\nby Andrey Karelskiy");
+        ctx.render("index.html");
     };
 }
