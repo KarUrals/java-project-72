5ddda03927b2d84375b05441db357ed26e64295b
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-02-02 14:39:25 +0200

    fix linter errors in Url.java

--- app/src/main/java/hexlet/code/domain/Url.java
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -3,7 +3,12 @@ package hexlet.code.domain;
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.Instant;
 import java.util.List;
 
